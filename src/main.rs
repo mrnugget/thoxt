066402fb5ea89c620d4be5dc7883313f9809bdec
Thorsten Ball <mrnugget@gmail.com>
2024-11-23 17:40:12 +0100

    Setup raw terminal mode

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,50 @@
-use libc;
-use std::io::{stdin, Read};
+use libc::{self, OLD_TIME};
+use std::{
+    io::{stdin, Read},
+    sync::Once,
+};
 
-unsafe fn enable_raw_mode() {
-    let mut old_attributes: libc::termios = std::mem::zeroed();
-    libc::tcgetattr(libc::STDIN_FILENO, &mut old_attributes);
+struct TerminalRawMode {
+    original_state: libc::termios,
+}
 
-    let mut new_attributes = old_attributes;
-    new_attributes.c_lflag &= libc::ECHO;
-    libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &new_attributes);
+impl TerminalRawMode {
+    unsafe fn enable() -> Self {
+        let mut old: libc::termios = std::mem::zeroed();
+        libc::tcgetattr(libc::STDIN_FILENO, &mut old);
+
+        let mut new = old;
+
+        new.c_iflag &= !(libc::BRKINT | libc::ICRNL | libc::INPCK | libc::ISTRIP | IXON);
+        new.c_oflag &= !(libc::OPOST);
+        new.c_cflag |= libc::CS8;
+        new.c_lflag &= !(libc::ECHO | libc::ICANON | libc::IEXTEN | libc::ISIG);
+
+        libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &new);
+
+        Self {
+            original_state: old,
+        }
+    }
 }
 
 fn main() {
-    unsafe { enable_raw_mode() };
-    let mut buf = [0; 1];
-    while let Ok(n) = stdin().read(&mut buf) {
-        println!("buf[0]: {:?}", buf[0]);
-        if n == 1 && buf[0] == 'q' as u8 {
-            return;
+    let _raw_mode = unsafe { TerminalRawMode::enable() };
+    let mut stdin = std::io::stdin();
+    let mut buffer = [0; 1];
+
+    while stdin.read_exact(&mut buffer).is_ok() {
+        let c = buffer[0] as char;
+        if c == 'q' {
+            break;
+        }
+
+        if c.is_control() {
+            // For control characters, just print the numeric value
+            println!("{}\r", buffer[0]);
+        } else {
+            // For regular characters, print both value and character
+            println!("{} ('{}')\r", buffer[0], c);
         }
     }
 }
